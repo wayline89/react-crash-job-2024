005f946269b10dc08032b5fc8f96d942e12c42f4
wayline89 <laurentdugardin@gmail.com>
2024-08-13 18:13:21 +0200

    3 avec pb de nom

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -6,8 +6,8 @@ const HomeCards = () => {
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-         <Card>
-         <h2 className="text-2xl font-bold">For Developers</h2>
+          <Card>
+            <h2 className="text-2xl font-bold">For Developers</h2>
             <p className="mt-2 mb-4">
               Browse our React jobs and start your career today
             </p>
@@ -17,10 +17,10 @@ const HomeCards = () => {
             >
               Browse Jobs
             </a>
-         </Card>
+          </Card>
 
-         <Card bg>
-         <h2 className="text-2xl font-bold">For Employers</h2>
+          <Card bg>
+            <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
             </p>
@@ -30,7 +30,7 @@ const HomeCards = () => {
             >
               Add Job
             </a>
-         </Card>
+          </Card>
         </div>
       </div>
     </section>
